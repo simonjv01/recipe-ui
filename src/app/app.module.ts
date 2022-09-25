c3e57015005796d73536c40b471c81eaa50160cc
Simon Vargas <sjvargas01@gmail.com>
2022-09-25 00:37:23 -0400

    created new recipe-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeFormComponent } from './recipe-form/recipe-form.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeFormComponent
+    RecipeFormComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
